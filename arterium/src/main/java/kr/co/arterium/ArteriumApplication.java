eefab67d3abf88008c3d6b5fd7cc202e17e60ce3
shj0212 <215212g@gmail.com>
2023-06-30 22:38:11 +0900

    프로젝트 셋팅

---
+++ arterium/src/main/java/kr/co/arterium/ArteriumApplication.java
@@ -0,0 +1,13 @@
+package kr.co.arterium;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArteriumApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArteriumApplication.class, args);
+    }
+
+}
---
+++ arterium/src/test/java/kr/co/arterium/ArteriumApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.arterium;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArteriumApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
