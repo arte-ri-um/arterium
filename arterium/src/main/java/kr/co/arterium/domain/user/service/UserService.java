8838b0400e09dc599a0411290203108f197ceccd
suisuiee <binnee7@hanmail.net>
2023-07-05 17:24:03 +0900

    squash! [FEAT] database ddl 수정

--- arterium/src/main/java/kr/co/arterium/domain/user/service/UserService.java
+++ arterium/src/main/java/kr/co/arterium/domain/user/service/UserService.java
@@ -1,4 +1,12 @@
 package kr.co.arterium.domain.user.service;
 
+import kr.co.arterium.domain.user.dto.AddUserRequestDTO;
+import kr.co.arterium.domain.user.entity.UserEntity;
+
+import java.util.Optional;
+
 public interface UserService {
+    Long signUp(AddUserRequestDTO dto);
+    Optional<UserEntity> findByUserId(Long id);
+    Optional<UserEntity> findUserByEmail(String email);
 }
