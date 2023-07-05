f41bfd82b89488da45a4f356d21179e3238dc9ca
suisuiee <binnee7@hanmail.net>
2023-07-05 17:21:24 +0900

    [FEAT] database ddl 수정
    - user -> users table 명 수정
    - int -> bigint 타입 수정
    - UserEntity 수정

--- arterium/src/main/java/kr/co/arterium/domain/user/entity/UserEntity.java
+++ arterium/src/main/java/kr/co/arterium/domain/user/entity/UserEntity.java
@@ -1,18 +1,20 @@
 package kr.co.arterium.domain.user.entity;
 
 import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.Collection;
 
 @Entity
-@Table(name = "user")
-@NoArgsConstructor
-@RequiredArgsConstructor
-@AllArgsConstructor
+@Table(name = "users")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
-@Builder
-public class UserEntity {
+public class UserEntity implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -37,21 +39,77 @@ public class UserEntity {
     @Column(length = 1, columnDefinition = "varchar(1) default 'N'")
     private String gender;
 
+    @Column(columnDefinition = "TEXT", name="profile_url")
     private String profileUrl;
 
-    @Column(columnDefinition = "int default 1")
+    @Column(columnDefinition = "int default 1", name="user_level")
     private int userLevel;
 
-    @Column(columnDefinition = "int default 30")
-    private int role;
+    @Column(columnDefinition = "int default 30", name="role")
+    private int roleId;
 
-    @Column(name="created_at", updatable = false)
-    @Temporal(TemporalType.TIMESTAMP)
-    @org.hibernate.annotations.CreationTimestamp
+    @CreationTimestamp
+    @Column(name = "created_at", updatable = false, nullable = false)
     private LocalDateTime createdAt;
 
-    @Column(name="updated_at")
-    @Temporal(TemporalType.TIMESTAMP)
-    @org.hibernate.annotations.UpdateTimestamp
+    @UpdateTimestamp
+    @Column(name = "updated_at", nullable = false)
     private LocalDateTime updatedAt;
+
+    // TODO - post 랑 연결
+
+    // == 생성 메서드 == /
+    @Builder(builderMethodName = "createUser")
+    public UserEntity(Long id, String password, String name, String nickname, String email, String phone, LocalDateTime birthdate, String gender, String profileUrl, int userLevel, int roleId, LocalDateTime createdAt, LocalDateTime updatedAt) {
+        this.id = id;
+        this.password = password;
+        this.name = name;
+        this.nickname = nickname;
+        this.email = email;
+        this.phone = phone;
+        this.birthdate = birthdate;
+        this.gender = gender;
+        this.profileUrl = profileUrl;
+        this.userLevel = userLevel;
+        this.roleId = roleId;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
+
+    // == 메서드
+    public void encryptPassword(String password){
+        this.password = password;
+    }
+
+
+    // == 스프링 시큐리티 ==/
+    @Override // 권한 반한
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override // 사용자의 id 반환
+    public String getUsername() {
+        return email;
+    }
+
+    @Override // 계정 만료 여부 반환
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override // 계정 잠금 여부 반환
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override // 패스워드 만료 여부 반환
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override // 계정 사용 가능 여부 반환
+    public boolean isEnabled() {
+        return true;
+    }
 }
