5e4876d6eb273e1291bc14efb83549f85c62c71d
suisuiee <binnee7@hanmail.net>
2023-07-05 17:25:45 +0900

    Revert "squash! [FEAT] database ddl 수정"
    This reverts commit 8838b0400e09dc599a0411290203108f197ceccd.

--- arterium/src/main/java/kr/co/arterium/domain/user/repository/UserRepository.java
+++ arterium/src/main/java/kr/co/arterium/domain/user/repository/UserRepository.java
@@ -1,18 +1,7 @@
 package kr.co.arterium.domain.user.repository;
 
-import kr.co.arterium.domain.user.dto.UserDTO;
 import kr.co.arterium.domain.user.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-import java.util.Optional;
-
 public interface UserRepository extends JpaRepository<UserEntity,Long> {
-    Optional<UserEntity> findByEmail(String email);
-
-    @Override
-    Optional<UserEntity> findById(Long aLong);
-
-    @Override
-    List<UserEntity> findAllById(Iterable<Long> longs);
 }
--- arterium/src/main/java/kr/co/arterium/domain/user/service/UserService.java
+++ arterium/src/main/java/kr/co/arterium/domain/user/service/UserService.java
@@ -1,12 +1,4 @@
 package kr.co.arterium.domain.user.service;
 
-import kr.co.arterium.domain.user.dto.AddUserRequestDTO;
-import kr.co.arterium.domain.user.entity.UserEntity;
-
-import java.util.Optional;
-
 public interface UserService {
-    Long signUp(AddUserRequestDTO dto);
-    Optional<UserEntity> findByUserId(Long id);
-    Optional<UserEntity> findUserByEmail(String email);
 }
