19744b759fc691c5686206d959e530d940c3c135
heahea7 <131749616+chaerimyoo1234@users.noreply.github.com>
2023-07-11 14:13:05 +0900

    [TEST] JWT - JWT 토큰 받아 일반 유저에 주도록 적용 - JWT Security 적용 - JWT Filter 적용 - tokenprovider 적용 - SecurityConfig에 jwt apply 추가 - UserController/ApiResponse에 token 추가

--- arterium/src/main/java/kr/co/arterium/domain/user/dto/UserDTO.java
+++ arterium/src/main/java/kr/co/arterium/domain/user/dto/UserDTO.java
@@ -1,9 +1,40 @@
 package kr.co.arterium.domain.user.dto;
 
 import lombok.Builder;
-import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
-@Data
-@Builder
+import java.time.LocalDateTime;
+
+@Getter
+@NoArgsConstructor
 public class UserDTO {
+    private Long id;
+    private String password;
+    private String nickname;
+    private String email;
+    private String phone;
+    private LocalDateTime birthdate;
+    private String gender;
+    private String profileUrl;
+    private int userLevel;
+    private int roleId;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    @Builder
+    public UserDTO(Long id, String password,String nickname, String email, String phone, LocalDateTime birthdate, String gender, String profileUrl, int userLevel, int role, LocalDateTime createdAt, LocalDateTime updatedAt) {
+        this.id = id;
+        this.password = password;
+        this.nickname = nickname;
+        this.email = email;
+        this.phone = phone;
+        this.birthdate = birthdate;
+        this.gender = gender;
+        this.profileUrl = profileUrl;
+        this.userLevel = userLevel;
+        this.roleId = role;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
 }
--- arterium/src/main/java/kr/co/arterium/domain/user/entity/UserEntity.java
+++ arterium/src/main/java/kr/co/arterium/domain/user/entity/UserEntity.java
@@ -1,27 +1,28 @@
 package kr.co.arterium.domain.user.entity;
 
 import lombok.*;
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.Collection;
 
 @Entity
-@Table(name = "user")
-@NoArgsConstructor
-@RequiredArgsConstructor
-@AllArgsConstructor
+@ToString
+@Table(name = "users")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
-@Builder
-public class UserEntity {
+public class UserEntity implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(nullable = false)
     private String password;
-
-    @Column(nullable = false, length = 20)
-    private String name;
 
     @Column(nullable = false, length = 10)
     private String nickname;
@@ -34,24 +35,87 @@ public class UserEntity {
 
     private LocalDateTime birthdate;
 
-    @Column(length = 1, columnDefinition = "varchar(1) default 'N'")
+    @Column(length = 1)
+    @ColumnDefault("N")
     private String gender;
 
+    @Column(columnDefinition = "TEXT", name = "profile_url")
     private String profileUrl;
 
-    @Column(columnDefinition = "int default 1")
+    @Column(name = "user_level")
     private int userLevel;
 
-    @Column(columnDefinition = "int default 30")
-    private int role;
+    @Column(name = "role_id")
+    private int roleId;
 
-    @Column(name="created_at", updatable = false)
-    @Temporal(TemporalType.TIMESTAMP)
-    @org.hibernate.annotations.CreationTimestamp
+    @CreationTimestamp
+    @Column(name = "created_at", updatable = false, nullable = false)
     private LocalDateTime createdAt;
 
-    @Column(name="updated_at")
-    @Temporal(TemporalType.TIMESTAMP)
-    @org.hibernate.annotations.UpdateTimestamp
+    @UpdateTimestamp
+    @Column(name = "updated_at", nullable = false)
     private LocalDateTime updatedAt;
+
+    // TODO - post 랑 연결
+
+    // == 생성 메서드 == /
+    @Builder(builderMethodName = "createUser")
+    public UserEntity(Long id, String password, String nickname, String email, String phone, LocalDateTime birthdate, String gender, String profileUrl, int userLevel, int roleId, LocalDateTime createdAt, LocalDateTime updatedAt) {
+        this.id = id;
+        this.password = password;
+        this.nickname = nickname;
+        this.email = email;
+        this.phone = phone;
+        this.birthdate = birthdate;
+        this.gender = gender;
+        this.profileUrl = profileUrl;
+        this.userLevel = userLevel;
+        this.roleId = roleId;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
+
+    @PrePersist
+    public void prePersist() {
+        this.gender = (this.gender == null) ? "N" : this.gender;
+        this.userLevel = ( this.userLevel == 0 ) ? 1 : this.userLevel;
+        this.roleId = (this.roleId == 0) ? 30 : this.roleId;
+    }
+
+    // == 메서드
+    public void encryptPassword(String password) {
+        this.password = password;
+    }
+
+
+    // == 스프링 시큐리티 ==/
+    @Override // 권한 반한
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override // 사용자의 id 반환
+    public String getUsername() {
+        return email;
+    }
+
+    @Override // 계정 만료 여부 반환
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override // 계정 잠금 여부 반환
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override // 패스워드 만료 여부 반환
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override // 계정 사용 가능 여부 반환
+    public boolean isEnabled() {
+        return true;
+    }
 }
