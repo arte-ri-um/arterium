1e4b34d4742233e171d524a44bcb87ad38c15e2e
suisuiee <binnee7@hanmail.net>
2023-07-12 14:30:06 +0900

    [ADD] 공통 뷰 추가 - 타임리프 레이아웃 제작 - 타임리프 fragment 분리 - 로그인, 회원가입 페이지 완료

--- arterium/src/main/java/kr/co/arterium/domain/user/controller/UserViewController.java
+++ arterium/src/main/java/kr/co/arterium/domain/user/controller/UserViewController.java
@@ -5,6 +5,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class UserViewController {
+    /**
+     * sign-in 로그인
+     * */
+    @RequestMapping("/users/sign-in")
+    public String signInForm(){
+        return "users/sign-in";
+    }
+
+    /**
+     * sign-up 회원가입
+     * @return
+     */
     @RequestMapping("/users/sign-up")
     public String signUpForm(){
         return "users/sign-up";
