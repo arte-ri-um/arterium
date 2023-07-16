da7304060994b431d4d6e5bb8134b502433e9114
shj0212 <215212g@gmail.com>
2023-07-16 21:52:51 +0900

    [FEAT&ADD] 포스트 수정, 삭제 구현, postEntity에 BookingLinkEntity cascade되도록 변경

--- arterium/src/main/java/kr/co/arterium/domain/exhibition/controller/PostController.java
+++ arterium/src/main/java/kr/co/arterium/domain/exhibition/controller/PostController.java
@@ -10,6 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.transaction.Transactional;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -55,5 +56,46 @@ public class PostController {
         modelAndView.addObject("postDTO",onePostDTO);   //모델 입력
         modelAndView.setViewName("exhibition/postView");    //페이지 셋팅
         return modelAndView;
+    }
+
+    @PostMapping("/post/{postId}/delete")
+    public ResponseEntity<Map<String,String>> deletePostById(@PathVariable("postId")Long postId){ //포스트 삭제 - 전체 리스트가 없어서 create로 되돌아감
+        postService.deleteById(postId);
+        //리다이렉트 URL을 응답에 포함시키기
+        Map<String,String> response = new HashMap<>();
+        response.put("redirectUrl","/artpost/create-post");
+
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/post/{postId}/edit")
+    public ModelAndView modifyPostViewById(@PathVariable("postId")Long postId,ModelAndView modelAndView){   // 포스트 수정 view
+        //post 정보 가져오기
+        PostViewDTO onePostDTO = postService.findPostById(postId);
+        modelAndView.addObject("postDTO",onePostDTO);
+
+        //전시장 정보 가져오기 TODO 장소별로 가져오기
+        List<ExhibitionDTO> exhibitions = postService.findExhibitionsList();
+        modelAndView.addObject("exhibitions",exhibitions);
+
+        //예매사이트 정보 가져오기
+        List<BookingSiteDTO> bookingsites = postService.findBookingSiteList();
+        modelAndView.addObject("bookingSites",bookingsites);
+
+        modelAndView.setViewName("exhibition/createPostView");    //페이지 셋팅
+        return modelAndView;
+    }
+
+    @PostMapping("/post/{postId}/edit")
+    public ResponseEntity<Map<String,String>> modifyPostById(
+            @PathVariable("postId")Long postId,@RequestBody PostDTO postDTO, ModelAndView modelAndView){    // 수정 저장
+        postDTO.setUserId(1L);  // TODO 아직 user 연동 안해서 세션해도 되는지 모르겠음
+        postService.modifyById(postDTO);    //변경내용저장
+
+        //리다이렉트 URL을 응답에 포함시키기
+        Map<String,String> response = new HashMap<>();
+        response.put("redirectUrl","/artpost/post/" + postId);
+
+        return ResponseEntity.ok(response);
     }
 }
--- arterium/src/main/java/kr/co/arterium/domain/exhibition/dto/BookingLinkDTO.java
+++ arterium/src/main/java/kr/co/arterium/domain/exhibition/dto/BookingLinkDTO.java
@@ -1,5 +1,6 @@
 package kr.co.arterium.domain.exhibition.dto;
 
+import kr.co.arterium.domain.exhibition.entity.PostEntity;
 import lombok.Builder;
 import lombok.Data;
 
@@ -10,4 +11,5 @@ public class BookingLinkDTO {   //예매 사이트 url 저장
     private Long postId;    //포스트id
     private Long siteId;    //예매사이트 id
     private String bookingUrl;  //예매 링크
+
 }
